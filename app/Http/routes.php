fb87cad5efbca00b9aeb92df809ea0ee970f20af
Magician <magicians.alex@gmail.com>
2016-05-30 10:28:26 +0800

    add route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,8 @@
 <?php
+/**
+ * 路由基本控制器
+ */
+Route::get('/','Homecontroller@index');
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+Route::get('/home','Homecontroller@home');
 
-Route::get('/', function () {
-    return view('welcome');
-});
